74187a95b538cd7a21ded585a07b9b737d8952f9
Michal Trubini <mchl.trubini@gmail.com>
2023-01-16 16:43:05 +0100

    background color of fixed header on scroll

--- src/modules/header/header.jsx
+++ src/modules/header/header.jsx
@@ -3,15 +3,37 @@ import useWindowDimensions from "../../util/WindowDimensions";
 import MobileView from "./mobile/mobileView";
 import DesktopView from "./desktopView";
 import Hero from "./hero";
+import { useEffect } from "react";
 
 const Header = () => {
   const { width } = useWindowDimensions();
   const mobileBreakpoint = 1024;
 
+  useEffect(() => {
+    const siteMainNav = document.getElementById("siteMainNav");
+    let baseScrollTop = 0;
+
+    function onScroll() {
+      let currentScrollPosition = window.pageYOffset;
+
+      if (currentScrollPosition > baseScrollTop) {
+        siteMainNav.classList.add("siteMainNav__fix");
+      } else {
+        siteMainNav.classList.remove("siteMainNav__fix");
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
     <header className={styles.header}>
       <div className="container">
-        <nav className={styles.nav}>{width < mobileBreakpoint ? <MobileView /> : <DesktopView />}</nav>
+        <nav id='siteMainNav' className={styles.nav}>{width < mobileBreakpoint ? <MobileView /> : <DesktopView />}</nav>
         <Hero />
       </div>
     </header>
