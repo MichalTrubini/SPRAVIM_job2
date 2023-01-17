07d86adb576c839513427d9d00279d172944cb59
Michal Trubini <mchl.trubini@gmail.com>
2023-01-17 09:21:29 +0100

    form validation + message on valid submit

--- src/modules/footer/footer.jsx
+++ src/modules/footer/footer.jsx
@@ -1,25 +1,71 @@
 import styles from "./footer.module.css";
-import facebookLogo from '../../assets/facebook-circle-line.svg';
-import instagramLogo from '../../assets/instagram-line.svg';
-
+import facebookLogo from "../../assets/facebook-circle-line.svg";
+import instagramLogo from "../../assets/instagram-line.svg";
+import { useState } from "react";
+import checkCircleImage from '../../assets/check-circle.svg'
 
 const Footer = () => {
+  const [email, setEmail] = useState({
+    email: "",
+    isValid: false,
+  });
+
+  function emailValidation() {
+    const regex = /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/;
+
+    return regex.test(email.email);
+  }
+
+  const formValidationHandler = (e) => {
+    e.preventDefault();
+
+    const nextFormState = {
+      ...email,
+      isValid: emailValidation(),
+    };
+    setEmail(nextFormState);
+  };
+
+  const inputHandler = (e) => {
+    const nextFormState = {
+      ...email,
+      [e.target.name]: e.target.value,
+    };
+    setEmail(nextFormState);
+  };
+
   return (
-    <footer id='footerID' className={`${styles.footer} sitePadding`}>
+    <footer id="footerID" className={`${styles.footer} sitePadding`}>
       <div className={styles.container}>
-        <form noValidate className={styles.form}>
-          <h2 className={styles.heading}>Poďte s nami do toho</h2>
-          <p className={styles.text}>
-            Čoskoro budete môcť využívať Skinekt aj na vlastnej koži. Zadajte svoj mail a buďte prví kto bude mať
-            možnosť využívať Skinekt. Vysoké zľavy pre vás.
-          </p>
-          <input className={styles.input} placeholder="Váš e-mail"/>
-          <button className={styles.button}>Potvrdiť</button>
-          <p className={styles.text}>Váš e-mail bude u nás v bezpečí.</p>
-        </form>
+        <div className={styles.wrapper}>
+          {email.isValid ? (
+            <div className={styles.confirmation}>
+              <img src={checkCircleImage} alt='checkmark' className={styles.imageCircle}/>
+              <h2 className={`${styles.heading} ${styles.headingSubmit}`}>Ďakujeme. Budeme vás informovať o novinkách.</h2>
+            </div>
+          ) : (
+            <form className={styles.form} onSubmit={formValidationHandler}>
+              <h2 className={styles.heading}>Poďte s nami do toho</h2>
+              <p className={styles.text}>
+                Čoskoro budete môcť využívať Skinekt aj na vlastnej koži. Zadajte svoj mail a buďte prví kto bude mať
+                možnosť využívať Skinekt. Vysoké zľavy pre vás.
+              </p>
+              <input
+                className={styles.input}
+                placeholder="Váš e-mail"
+                name="email"
+                onChange={inputHandler}
+                value={email.email}
+              />
+              <button className={styles.button}>Potvrdiť</button>
+              <p className={styles.gdpr}>Váš e-mail bude u nás v bezpečí.</p>
+            </form>
+          )}
+        </div>
+
         <div className={styles.social}>
-            <img src={facebookLogo} alt='facebook'/>
-            <img src={instagramLogo} alt='instagram'/>
+          <img src={facebookLogo} alt="facebook" />
+          <img src={instagramLogo} alt="instagram" />
         </div>
       </div>
       <p className={styles.copyright}>© 2023 Skinekt - Power of elektronics in textile</p>
