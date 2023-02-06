a24096bbda93198e183e0fd740fa9b333d52ff19
Michal Trubini <mchl.trubini@gmail.com>
2023-02-06 22:14:50 +0100

    animation WIP

--- src/modules/main/homePage/highlights.jsx
+++ src/modules/main/homePage/highlights.jsx
@@ -2,18 +2,11 @@ import styles from "./highlights.module.css";
 import zdravieImage from "../../../assets/zdravie@2x.webp";
 import sportImage from "../../../assets/sport@2x.webp";
 import lifestyleImage from "../../../assets/lifestyle@2x.webp";
-import useWindowDimensions from "../../../util/WindowDimensions";
 import { useEffect, useState } from "react";
 
 const Highlights = () => {
-  const { width } = useWindowDimensions();
-  const [positionParent, setPositionParent] = useState(0);
-  const [positionHighlights, setPositionHighlights] = useState(0);
-  const [positionImageOne, setPositionImageOne] = useState(0);
-  const [positionImageTwo, setPositionImageTwo] = useState(0);
-  const [elementHeight, setElementHeight] = useState(0);
-  const [headerHeight, setHeaderHeight] = useState(0);
-  const headerWidth = width * 0.44;
+  const [tresholdImageOne, setThresholdImageOne] = useState(false);
+  const [tresholdImageTwo, setThresholdImageTwo] = useState(false);
 
   const scrollHandler = (item) => {
     const element = document.getElementById(item);
@@ -32,167 +25,134 @@ const Highlights = () => {
   };
 
   useEffect(() => {
-    const scrollableSection = document.getElementById("scrollID");
-    const highlightsSection = document.getElementById("highlightsID");
-    const header = document.getElementById("siteMainNav");
     const imageOne = document.getElementById("imageContainerOne");
     const imageTwo = document.getElementById("imageContainerTwo");
 
-    //set element height
+    //set header height
 
-    setElementHeight(imageOne.getBoundingClientRect().height);
-    setHeaderHeight(header.getBoundingClientRect().height);
+    const headerHeight = 74;
 
-    //get position of elements
+    //get threshold of elements
 
     window.addEventListener("scroll", handleScroll);
-    scrollableSection.addEventListener("scroll", handleScroll);
     return () => {
-      scrollableSection.removeEventListener("scroll", handleScroll);
       window.removeEventListener("scroll", handleScroll);
     };
 
     function handleScroll() {
-      const observerOne = new IntersectionObserver(handleIntersection);
-      const observerTwo = new IntersectionObserver(handleIntersection);
-      const observerThree = new IntersectionObserver(handleIntersection);
-      const observerFour = new IntersectionObserver(handleIntersection);
+      const imageOneTop =
+        imageOne.getBoundingClientRect().top + imageOne.getBoundingClientRect().height / 2 - headerHeight;
+      const imageTwoTop = imageTwo.getBoundingClientRect().top - headerHeight;
 
-      function handleIntersection(entries) {
-        const entry = entries[0];
-        if (entry.target.id === "scrollID")
-          setPositionParent(entry.boundingClientRect.top + entry.boundingClientRect.height / 2);
-        if (entry.target.id === "imageContainerOne")
-          setPositionImageOne(entry.boundingClientRect.top + entry.boundingClientRect.height);
-        if (entry.target.id === "imageContainerTwo")
-          setPositionImageTwo(entry.boundingClientRect.top + entry.boundingClientRect.height);
-        if (entry.target.id === "highlightsID") setPositionHighlights(entry.boundingClientRect.top - headerHeight);
-      }
-
-      observerOne.observe(scrollableSection);
-      observerTwo.observe(imageOne);
-      observerThree.observe(imageTwo);
-      observerFour.observe(highlightsSection);
+      if (imageOneTop < 0) setThresholdImageOne(true);
+      else setThresholdImageOne(false);
+      if (imageTwoTop < 0) setThresholdImageTwo(true);
+      else setThresholdImageTwo(false);
     }
-  }, [positionParent, positionImageOne, positionImageTwo, positionHighlights, headerHeight]);
+  }, []);
+
+  console.log(tresholdImageTwo);
 
   useEffect(() => {
     //logic that controls animation in the highlights section on scroll
 
     const sliderElements = document.querySelectorAll('[data-act="animate"]');
-    if (positionImageOne - positionParent < 0) {
+
+    if (tresholdImageOne === false) sliderElements[0].classList.add("isActive");
+    else {
       sliderElements[0].classList.remove("isActive");
-    } else {
-      sliderElements[0].classList.remove("isPrev");
-      sliderElements[0].classList.add("isActive");
     }
 
-    if (
-      sliderElements[1].previousElementSibling.classList.contains("isActive") ||
-      positionImageTwo - positionParent < 0
-    )
+    if (sliderElements[1].previousElementSibling.classList.contains("isActive"))
       sliderElements[1].classList.remove("isActive");
-    else if (positionImageTwo - positionParent > 0) {
-      sliderElements[1].classList.add("isActive");
-    }
+    else if (tresholdImageTwo === false) sliderElements[1].classList.add("isActive");
+    else sliderElements[1].classList.remove("isActive");
 
-    if (
-      sliderElements[1].previousElementSibling.classList.contains("isActive") ||
-      sliderElements[2].previousElementSibling.classList.contains("isActive")
-    )
-      sliderElements[2].classList.remove("isActive");
-    else sliderElements[2].classList.add("isActive");
-  });
+    if (tresholdImageTwo === true) sliderElements[2].classList.add("isActive");
+    else sliderElements[2].classList.remove("isActive");
+  }, [tresholdImageOne, tresholdImageTwo]);
 
+  const paragraphOne = (
+    <>
+      <h3 className={styles.textHeading}>šport</h3>
+      <p className={styles.text}>
+        Veľmi presne monitorujme polohu tela športovca, jeho fyziologické hodnoty a celkový stav. Tieto dáta
+        transformujeme na informácie, ktoré pomáhajú športovcom zlepšovať ich výkon a techniku.
+      </p>
+    </>
+  );
+
+  const paragraphTwo = (
+    <>
+      <h3 className={styles.textHeading}>lifestyle</h3>
+      <p className={styles.text}>
+        Či už pracujete dlhú dobu za počítačom, alebo len chcete vedieť o sebe viac pre zlepšenie zdravia, tak Skinekt
+        vám v tomto smere pomáha získavaním dát a ich spracovaním a vyhodnocovaním.
+      </p>
+    </>
+  );
+
+  const paragraphThree = (
+    <>
+      <h3 className={styles.textHeading}>zdravie</h3>
+      <p className={styles.text}>
+        Práve v tejto oblasti má Skinekt obrovské využitie. Lekár vie dokonale vyhodocovať dáta o pacientovi aj na veľkú
+        vzdialenosť, viete byť informovaný o stave seniorov a pod.
+      </p>
+    </>
+  );
   return (
-    <div style={{ position: "relative" }}>
-      <div
-        style={{ width: `${headerWidth}px`, height: `${elementHeight}px` }}
-        className={`${styles.header} ${styles.headerDesktop} sitePadding`}
-      >
-        <div>
-          <h2 className={styles.heading}>Pre koho je Skinekt prínosom</h2>
-          <div
-            className={`${styles.link} ${styles.linkDesktop}`}
-            onClick={() => {
-              scrollHandler("footerID");
-            }}
-          >
-            <p className={styles.linkText}>Zaujíma ma to</p>
-          </div>
-        </div>
-        <div className={styles.description}>
-          <div className={`${styles.textContainer} ${styles.textContainerDT} isActive`} data-act="animate">
-            <h3 className={styles.textHeading}>šport</h3>
-            <p className={styles.text}>
-              Veľmi presne monitorujme polohu tela športovca, jeho fyziologické hodnoty a celkový stav. Tieto dáta
-              transformujeme na informácie, ktoré pomáhajú športovcom zlepšovať ich výkon a techniku.
-            </p>
-          </div>
-          <div className={`${styles.textContainer} ${styles.textContainerDT}`} data-act="animate">
-            <h3 className={styles.textHeading}>lifestyle</h3>
-            <p className={styles.text}>
-              Či už pracujete dlhú dobu za počítačom, alebo len chcete vedieť o sebe viac pre zlepšenie zdravia, tak
-              Skinekt vám v tomto smere pomáha získavaním dát a ich spracovaním a vyhodnocovaním.
-            </p>
-          </div>
-
-          <div className={`${styles.textContainer} ${styles.textContainerDT}`} data-act="animate">
-            <h3 className={styles.textHeading}>zdravie</h3>
-            <p className={styles.text}>
-              Práve v tejto oblasti má Skinekt obrovské využitie. Lekár vie dokonale vyhodocovať dáta o pacientovi aj na
-              veľkú vzdialenosť, viete byť informovaný o stave seniorov a pod.{" "}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div id="highlightsID" className={styles.sectionWrapper} >
-        <section
-          id="scrollID"
-          className={styles.highlights}
-        >
-          <div className={`${styles.container} sitePadding`}>
-            <div className={styles.leftWrapper}>
-              <div className={`${styles.header} ${styles.headerMobile}`}>
-                <h2 className={styles.heading}>Pre koho je Skinekt prínosom</h2>
+    <section id="highlightsID" className={styles.section}>
+      <div className={`${styles.sectionWrapper} sitePadding`}>
+        <div className={styles.leftWrapper}>
+          <div className={styles.description}>
+            <div>
+              <h2 className={styles.heading}>Pre koho je Skinekt prínosom</h2>
+              <div
+                className={`${styles.link} ${styles.linkDesktop}`}
+                onClick={() => {
+                  scrollHandler("footerID");
+                }}
+              >
+                <p className={styles.linkText}>Zaujíma ma to</p>
               </div>
+            </div>
+            <div className={styles.animatedContent}>
+              <div className={`${styles.textContainer} ${styles.textContainerDT}`} data-act="animate">
+                {paragraphOne}
+              </div>
+              <div className={`${styles.textContainer} ${styles.textContainerDT}`} data-act="animate">
+                {paragraphTwo}
+              </div>
+              <div className={`${styles.textContainer} ${styles.textContainerDT}`} data-act="animate">
+                {paragraphThree}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="scrollID" className={styles.highlights}>
+          <div className={styles.container}>
+            <div className={styles.headerMobile}>
+              <h2 className={styles.heading}>Pre koho je Skinekt prínosom</h2>
             </div>
             <div className={styles.wrapper}>
               <div className={styles.highlight}>
                 <div id="imageContainerOne" className={styles.imageContainer}>
                   <img src={sportImage} alt="sport" />
                 </div>
-                <div className={styles.textContainer}>
-                  <h3 className={styles.textHeading}>šport</h3>
-                  <p className={styles.text}>
-                    Veľmi presne monitorujme polohu tela športovca, jeho fyziologické hodnoty a celkový stav. Tieto dáta
-                    transformujeme na informácie, ktoré pomáhajú športovcom zlepšovať ich výkon a techniku.
-                  </p>
-                </div>
+                <div className={styles.textContainer}>{paragraphOne}</div>
               </div>
               <div className={styles.highlight}>
                 <div id="imageContainerTwo" className={styles.imageContainer}>
                   <img src={lifestyleImage} alt="lifestyle" />
                 </div>
-                <div className={styles.textContainer}>
-                  <h3 className={styles.textHeading}>lifestyle</h3>
-                  <p className={styles.text}>
-                    Či už pracujete dlhú dobu za počítačom, alebo len chcete vedieť o sebe viac pre zlepšenie zdravia,
-                    tak Skinekt vám v tomto smere pomáha získavaním dát a ich spracovaním a vyhodnocovaním.
-                  </p>
-                </div>
+                <div className={styles.textContainer}>{paragraphTwo}</div>
               </div>
               <div className={styles.highlight}>
                 <div id="imageContainerThree" className={styles.imageContainer}>
                   <img src={zdravieImage} alt="zdravie" />
                 </div>
-                <div className={styles.textContainer}>
-                  <h3 className={styles.textHeading}>zdravie</h3>
-                  <p className={styles.text}>
-                    Práve v tejto oblasti má Skinekt obrovské využitie. Lekár vie dokonale vyhodocovať dáta o pacientovi
-                    aj na veľkú vzdialenosť, viete byť informovaný o stave seniorov a pod.{" "}
-                  </p>
-                </div>
+                <div className={styles.textContainer}>{paragraphThree}</div>
               </div>
             </div>
 
@@ -205,9 +165,9 @@ const Highlights = () => {
               <p className={styles.linkText}>Zaujíma ma to</p>
             </div>
           </div>
-        </section>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
